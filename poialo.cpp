474ec46eaa17c71f6bd5b09755c5c6f5deef55a3
Geophagus96 <33130813+Geophagus96@users.noreply.github.com>
2018-08-03 03:11:47 +0800

    Add files via upload

---
+++ poialo.cpp
@@ -0,0 +1,44 @@
+#include <RcppArmadillo.h>
+#include <math.h>
+using namespace Rcpp;
+using namespace arma;
+using namespace std;
+//[[Rcpp::plugins(cpp11)]]
+//[[Rcpp::depends(RcppArmadillo)]]
+
+
+//[[Rcpp::export]]
+
+vec poilassoALO(const vec &beta, const mat &X, const vec &y, const double &lambda){
+  vec theta = y - exp(X*beta);
+  double n = X.n_rows;
+  double lambda1 = n*lambda;
+  vec yhat = X*beta;
+  vec l2 = 1/(y-theta);
+  vec K = sqrt(l2);
+  vec yu = (theta%l2+yhat)/K;
+  mat Xu = diagmat((1/K))*X;
+  uvec E = find(abs(X.t()*theta)>lambda1);
+  mat Xue = Xu.cols(E);
+  mat J = eye<mat>(X.n_rows,X.n_rows) - Xue*inv_sympd(Xue.t()*Xue)*Xue.t();
+  vec yalo = K%(yu - K%theta/diagvec(J));
+  return yalo;
+}
+
+//[[Rcpp::export]]
+vec poielnetALO(const vec &beta, const mat &X, const vec &y, const double &lambda, const double &alpha){
+  double n = X.n_rows;
+  double lambda1 = n*lambda*alpha;
+  double lambda2 = n*lambda*(1-alpha)/2;
+  vec theta = y - exp(X*beta);
+  vec yhat = X*beta;
+  vec l2 = 1/(y-theta);
+  vec K = sqrt(l2);
+  vec yu = (theta%l2+yhat)/K;
+  mat Xu = diagmat((1/K))*X;
+  uvec E = find(abs(X.t()*theta)>lambda1);
+  mat Xue = Xu.cols(E);
+  mat J = inv_sympd(eye<mat>(X.n_rows,X.n_rows)+(1/(2*lambda2))*Xue*Xue.t());
+  vec yalo = K%(yu - K%theta/diagvec(J));
+  return yalo;
+}
